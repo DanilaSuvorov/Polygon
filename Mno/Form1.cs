f2a9ae5707d2d51550f222ff6ad1f8d0a42a05f4
Данила Суворов <dansuvtim@gmail.com>
2020-10-29 13:47:40 +0300

    drawing

--- Mno/Form1.cs
+++ Mno/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,13 +13,13 @@ namespace Mno
 {
     public partial class Form1 : Form
     {
-        MyShape shape;
-        int x, y;
         static int R;
-        int sshape;
         static Color col;
+        MyShape shape;
+        int sshape;
+        int x, y;
         int delX, delY;
-        bool Draw = false;
+        bool Draw = false; //существование вершины
         bool Dragger = false;
         static Form1()
         {
@@ -28,11 +29,14 @@ namespace Mno
         public Form1()
         {
             InitializeComponent();
+            delX = 0;
+            delY = 0;
+            Dragger = false;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            if (Draw) shape.Draw(e.Graphics);
+            if(Draw) shape.Draw(e.Graphics);
         }
         private void треугольникToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -43,44 +47,54 @@ namespace Mno
         {
             sshape = 2;
         }
-
+        private void кругToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sshape = 3;
+        }
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            shape.SetX = e.X + delX;
-            shape.SetY = e.Y + delY;
+            if (Dragger)
+            {
+                shape.SetX = e.X + delX;
+                shape.SetY = e.Y + delY;
+            }
             this.Invalidate();
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
             Dragger = false;
-        }
-
-        private void кругToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            sshape = 3;
+            this.Invalidate();
         }
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             if (Draw && shape.Check(e.X, e.Y))
             {
-                //e.Button = MouseButtons.Left;
-                //флаг на перетасиквание
-                Dragger = true;
-                // фиксируем расстояние 
-                delX = e.X - shape.SetX;
-                delY = e.Y - shape.SetY;
+                if (e.Button != MouseButtons.Left) 
+                {
+                    // фиксируем расстояние 
+                    delX = e.X - shape.SetX;
+                    delY = e.Y - shape.SetY;
+                    Dragger = true;
+                }
+                else if (e.Button != MouseButtons.Right)
+                {
+                    this.Invalidate();
+                }
             }
             else
             {
                 Draw = true;
                 switch (sshape)
                 {
-                    case 0: shape = new Circle(e.X, e.Y); break;
+                    case 1: shape = new Triangle(e.X, e.Y); break;
+                    case 2: shape = new Square(e.X, e.Y); break;
+                    case 3: shape = new Circle(e.X, e.Y); break;
                     default: shape = new Circle(e.X, e.Y); break;
                 }
             }
-
+            this.Invalidate();
+            //this.Refresh();
         }
     }
 
@@ -100,9 +114,15 @@ namespace Mno
             y0 = 100;
         }
         public MyShape(int x, int y)
-        { }
+        {
+            this.x0 = x;
+            this.y0 = y;
+        }
         static MyShape()
-        { }
+        {
+            r = 50;
+            c = Color.Green;
+        }
 
         public abstract void Draw(Graphics g);
         public abstract bool Check(int x, int y);
@@ -130,7 +150,7 @@ namespace Mno
 
         override public void Draw(Graphics g)
         {
-            //g.FillEllipse(new SolidBrush(c), x0 - r, y0 - r, 2 * r, 2 * r);
+            g.FillRectangle(new SolidBrush(c), x0 - r, y0 - r, 2 * r, 2 * r);
         }
 
         override public bool Check(int x, int y)
@@ -146,7 +166,11 @@ namespace Mno
 
         override public void Draw(Graphics g)
         {
-            //g.FillEllipse(new SolidBrush(c), x0 - r, y0 - r, 2 * r, 2 * r);
+            Point points1 = new Point(x0 - r, y0 - r / 2);
+            Point points2 = new Point(x0, y0 + r / 2);
+            Point points3 = new Point(x0 + r, y0 - r / 2);
+            Point[] curvePoints = { points1, points2, points3 };
+            g.FillPolygon(new SolidBrush(c), curvePoints);
         }
 
         override public bool Check(int x, int y)
