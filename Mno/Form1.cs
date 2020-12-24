d8ec7576c5ab923a917e08046fe453f8527e8adb
Данила Суворов <dansuvtim@gmail.com>
2020-12-24 15:06:02 +0300

    normal

--- Mno/Form1.cs
+++ Mno/Form1.cs
@@ -1,182 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Mno
 {
     public partial class Form1 : Form
     {
-        static int R;
-        static Color col;
-        MyShape shape;
-        int sshape;
-        int x, y;
-        int delX, delY;
-        bool Draw = false; //существование вершины
-        bool Dragger = false;
-        static Form1()
-        {
-            R = 70;
-            col = Color.Green;
-        }
+        int shape = 1;
+        Mno.Vertex shaper;
+        List<Mno.Vertex> vert = new List<Mno.Vertex>();
+
+        int x1 = 100;
+        int i = 0;
+        int y1 = 100;
+        bool above;
+        bool below;
+
         public Form1()
         {
+            shaper = new Mno.circle(0, 0);
+
             InitializeComponent();
-            delX = 0;
-            delY = 0;
-            Dragger = false;
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x1 = e.X;
+            y1 = e.Y;
+            if (e.Button == MouseButtons.Left)
+            {
+                if (shape == 1)
+                {
+                    shaper = new Mno.circle(y1, x1);
+
+                    vert.Add(shaper);
+                }
+                else
+                {
+                    if (shape == 2)
+                    {
+                        shaper = new Mno.square(y1, x1);
+
+                        vert.Add(shaper);
+                    }
+                    if (shape == 3)
+                    {
+                        shaper = new Mno.triangle(y1, x1);
+
+                        vert.Add(shaper);
+                    }
+                }
+                vert[i].existance = 1;
+                this.Invalidate();
+                i++;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (vert[j].check(e.X, e.Y)) vert[j].existance = 0;
+                }
+                this.Invalidate();
+            }
+        }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            if(Draw) shape.Draw(e.Graphics);
-        }
-        private void треугольникToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            sshape = 1;
-        }
+            foreach (Mno.Vertex shaper in vert)
+            {
+                shaper.l = false;
+                if (shaper.existance == 1)
+                    shaper.paintshit(e.Graphics);
+            }
+            for (int i = 0; i < vert.Count; i++)
+            {
+                for (int j = i + 1; j < vert.Count; j++)
+                {
+                    above = false;
+                    below = false;
 
-        private void квадратToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            sshape = 2;
+                    for (int k = 0; k < vert.Count; k++)
+                    {
+                        if (k != i && k != j && i != j)
+                        {
+
+                            if (((vert[k].y1 - vert[i].y1) * (vert[j].x1 - vert[i].x1)) > ((vert[k].x1 - vert[i].x1) * (vert[j].y1 - vert[i].y1)))
+                                above = true;
+                            else below = true;
+                        }
+                    }
+                    if(!above || !below)
+                    {
+                        vert[i].l = true;
+                        vert[j].l = true;
+                        e.Graphics.DrawLine(new Pen(Color.Black), vert[i].x1, vert[i].y1, vert[j].x1, vert[j].y1);
+                    }
+                }
+            }
         }
         private void кругToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            sshape = 3;
+            shape = 1;
+        }
+        private void квадратToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            shape = 2;
+
+        }
+        private void треугольникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 3;
+
         }
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (Dragger)
+            for (int j = 0; j < i; j++)
             {
-                shape.SetX = e.X + delX;
-                shape.SetY = e.Y + delY;
-            }
-            this.Invalidate();
-        }
-
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
-        {
-            Dragger = false;
-            this.Invalidate();
-        }
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (Draw && shape.Check(e.X, e.Y))
-            {
-                if (e.Button != MouseButtons.Left) 
+                if (e.Button == MouseButtons.Left && vert[j].check(e.X, e.Y))
                 {
-                    // фиксируем расстояние 
-                    delX = e.X - shape.SetX;
-                    delY = e.Y - shape.SetY;
-                    Dragger = true;
-                }
-                else if (e.Button != MouseButtons.Right)
-                {
+                    vert[j].x1 = e.X;
+                    vert[j].y1 = e.Y; 
                     this.Invalidate();
                 }
             }
-            else
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < vert.Count; i++)
             {
-                Draw = true;
-                switch (sshape)
+                if (!vert[i].l)
                 {
-                    case 1: shape = new Triangle(e.X, e.Y); break;
-                    case 2: shape = new Square(e.X, e.Y); break;
-                    case 3: shape = new Circle(e.X, e.Y); break;
-                    default: shape = new Circle(e.X, e.Y); break;
+                    vert[i].existance = 0;
+                    this.Invalidate();
                 }
+                this.Invalidate();
             }
-            this.Invalidate();
-            //this.Refresh();
-        }
-    }
-
-    abstract class MyShape
-    {
-        protected int x0;
-        protected int y0;
-        public static int r { set; get; }
-        public static Color c { set; get; }
-
-        public int SetX { set { x0 = value; } get { return x0; } }
-        public int SetY { set { y0 = value; } get { return y0; } }
-
-        public MyShape()
-        {
-            x0 = 100;
-            y0 = 100;
-        }
-        public MyShape(int x, int y)
-        {
-            this.x0 = x;
-            this.y0 = y;
-        }
-        static MyShape()
-        {
-            r = 50;
-            c = Color.Green;
-        }
-
-        public abstract void Draw(Graphics g);
-        public abstract bool Check(int x, int y);
-    }
-    class Circle : MyShape
-    {
-        public Circle() : base() { }
-        public Circle(int x, int y) : base(x, y) { }
-
-        override public void Draw(Graphics g)
-        { 
-            g.FillEllipse(new SolidBrush(c), x0 - r, y0 - r, 2 * r, 2 * r);
-        }
-
-        override public bool Check(int x, int y) 
-        {
-            //посчитать катеты, сравнить гипотенузу
-            return true;
-        }
-    }
-    class Square : MyShape 
-    {
-        public Square() : base() { }
-        public Square(int x, int y) : base(x, y) { }
-
-        override public void Draw(Graphics g)
-        {
-            g.FillRectangle(new SolidBrush(c), x0 - r, y0 - r, 2 * r, 2 * r);
-        }
-
-        override public bool Check(int x, int y)
-        {
-            //посчитать 
-            return true;
-        }
-    }
-    class Triangle : MyShape 
-    {
-        public Triangle() : base() { }
-        public Triangle(int x, int y) : base(x, y) { }
-
-        override public void Draw(Graphics g)
-        {
-            Point points1 = new Point(x0 - r, y0 - r / 2);
-            Point points2 = new Point(x0, y0 + r / 2);
-            Point points3 = new Point(x0 + r, y0 - r / 2);
-            Point[] curvePoints = { points1, points2, points3 };
-            g.FillPolygon(new SolidBrush(c), curvePoints);
-        }
-
-        override public bool Check(int x, int y)
-        {
-            //посчитать
-            return true;
         }
     }
 }
